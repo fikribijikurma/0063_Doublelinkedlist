8c2a8c082b0dafcdc42ff6117b7501ada110ef49
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-04 01:38:30 -0700

    tampilkan looping

--- 0063_Doublelinkedlist.cpp
+++ 0063_Doublelinkedlist.cpp
@@ -8,13 +8,32 @@ struct Node
 
 Node* START = NULL;
 
-void addNode() {
+void addNode()
+{
 	Node* newNode = new Node(); // step 1:create a new node
 	cout << "\nEnter the roll number the student: ";
 	cin >> newNode->noMhs; // Assign value to the data field of the new node
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name; // Assign value to the data field of the new node
 
-	
+	// insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs) 
+	{ // step 2: insert the new node at the beginning
 
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mduplikate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+		// if the list empty, make the new node the START
+		newNode->next = START; // step 3: make the node node point to the first node
+		if (START = newNode)
+		{
+			START->prev = newNode; // step 4: make the first node point to the node
+		}
+		newNode->prev = NULL; // step 5: make the new node point to NULL
+		START = newNode;     //  step 6: make the new node the first node
 }
+else
+	{
+		// 
