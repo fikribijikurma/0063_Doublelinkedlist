8f2cd330f32fdafef0234caac47db997c41bea2c
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-04 03:14:15 -0700

    tampilkan menu pada while

--- 0063_Doublelinkedlist.cpp
+++ 0063_Doublelinkedlist.cpp
@@ -175,3 +175,22 @@ void revtraverse()
 		cout << "\nName: " << curr->name << endl;
 	}
 }
+
+int main()
+{
+	while (true) 
+	{
+		try
+		{
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all record in the ascending order of roll numbers" << endl;
+			cout << "4. View all record in the descending order of roll numbers" << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): " << endl;
+			
+		}
+	}
+}
