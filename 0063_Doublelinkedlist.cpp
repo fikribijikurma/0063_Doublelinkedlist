69bb7f000963910db35a99ce00c4881f875c1e22
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-04 02:13:00 -0700

    masukkan while

--- 0063_Doublelinkedlist.cpp
+++ 0063_Doublelinkedlist.cpp
@@ -68,6 +68,13 @@ void addNode()
 
 bool search(int rollNo, Node** previous, Node** current)
 {
-
+	*previous = NULL;
+	*current = START;
+	while (*current != NULL && (*current) - .noMhs != rollNo)
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	
 }
 
