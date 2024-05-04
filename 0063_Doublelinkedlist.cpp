de93b013880e1e7adb346860b7ad6f6a9bf6fbbc
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-04 01:22:47 -0700

    tampilkan cout pada void addnode

--- 0063_Doublelinkedlist.cpp
+++ 0063_Doublelinkedlist.cpp
@@ -1,6 +1,20 @@
-struct Node {
+struct Node 
+{
 	int noMhs;
 	string name;
-	Node* next;
-	Node* prev;
+	Node *next;
+	Node *prev;
 };
+
+Node* START = NULL;
+
+void addNode() {
+	Node* newNode = new Node(); // step 1:create a new node
+	cout << "\nEnter the roll number the student: ";
+	cin >> newNode->noMhs; // Assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // Assign value to the data field of the new node
+
+	
+
+}
